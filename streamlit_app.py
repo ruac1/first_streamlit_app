321b94994470b09a2d276f7e3721408ca95588da
Andrew de Ruiter <111131518+ruac1@users.noreply.github.com>
2022-08-12 16:28:54 +0200

    Update streamlit_app.py
    add text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+stremlit.text('Omega 3  Blueberre Oatmeal')
+stremlit.text('Kale, Spinach & Rocket Smoothie')
+stremlit.text('Hard-Boiled Free-Range Egg')
